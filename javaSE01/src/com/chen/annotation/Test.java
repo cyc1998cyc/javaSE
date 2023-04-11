532630e8414003bcdcbed2537d6f35d5d4ca862b
cyc1998cyc <3502104521@qq.com>
2023-04-11 17:09:07 +0800

    测试提交

---
+++ javaSE01/src/com/chen/annotation/Test.java
@@ -0,0 +1,13 @@
+package com.chen.annotation;
+
+/**
+ * @author cyc
+ * @createTime 2023/4/11 17:07
+ * @description
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("jiansheng");
+    }
+}
