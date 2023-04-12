5e2f0c692617c4b6993989f424ee23f954c9c1d5
cyc1998cyc <3502104521@qq.com>
2023-04-12 22:38:47 +0800

    测试提交

--- javaSE01/src/com/chen/annotation/Test.java
+++ javaSE01/src/com/chen/annotation/Test.java
@@ -8,6 +8,7 @@ package com.chen.annotation;
 
 public class Test {
     public static void main(String[] args) {
+        System.out.println("test");
         System.out.println("jiansheng");
     }
 }
