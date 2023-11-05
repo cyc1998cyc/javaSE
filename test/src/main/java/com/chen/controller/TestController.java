cadc3f16c3306525dd89b5c292283e3a7232f628
cyc1998cyc <3502104521@qq.com>
2023-11-05 22:54:43 +0800

    工作本提交

--- test/src/main/java/com/chen/controller/TestController.java
+++ test/src/main/java/com/chen/controller/TestController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @RequestMapping("/hello")
     public String hello(){
+        System.out.println("hello");
         return "hello springboot docker!";
     }
 }
